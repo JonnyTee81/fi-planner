18e4cab5e32b904029d7924cb3e44a0cad18236b
JonnyTee81 <jthompson81@gmail.com>
2019-11-26 20:55:34 -0600

    Created future value calculator

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-module.exports = function() {
-  return 'hello';
+module.exports = {
+  getTargetRetirementAmount: function(yearlyIncome) {
+    return yearlyIncome / 0.04;
+  },
+  getFutureValueWithYearlyDeposit: function(pv, pmt, rate, np) {
+    rate = parseFloat(rate); // Annual interest rate
+    np = parseFloat(np); // Number of periods/years
+    pmt = parseFloat(pmt); // Payment per period/year
+    pv = parseFloat(pv); // Present value
+    if (np == 0) {
+      alert('Why do you want to test me with zeros?');
+      return 0;
+    }
+    if (rate == 0) {
+      // Interest rate is 0
+      fv_value = pv + pmt * np;
+    } else {
+      x = Math.pow(1 + rate, np);
+      fv_value = (-pmt + x * pmt + rate * x * pv) / rate;
+    }
+    fv_value = conv_number(fv_value, 2);
+    return fv_value;
+  }
 };
+
+function conv_number(expr, decplaces) {
+  var str = '' + Math.round(eval(expr) * Math.pow(10, decplaces));
+  while (str.length <= decplaces) {
+    str = '0' + str;
+  }
+  var decpoint = str.length - decplaces;
+  return str.substring(0, decpoint) + '.' + str.substring(decpoint, str.length);
+}
--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,20 @@
 const assert = require('chai').assert;
 const app = require('../app');
 
-describe('App', () => {
-  it('app should return hello', () => {
-    assert.equal(app(), 'hello');
+// sayHelloResult = app.sayHello();
+// addNumbersResult = app.addNumbers(5, 5);
+
+describe('Finance', () => {
+  describe('Retirement', () => {
+    it('Should return a target retirement number of $2,500,000', () => {
+      assert.equal(app.getTargetRetirementAmount(100000), 2500000);
+    });
+
+    it('Should return a future value (FV) of $1,123', () => {
+      assert.equal(
+        app.getFutureValueWithYearlyDeposit(1000, 10, 0.05, 2),
+        1123
+      );
+    });
   });
 });
